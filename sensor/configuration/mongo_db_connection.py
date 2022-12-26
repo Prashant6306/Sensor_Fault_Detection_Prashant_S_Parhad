5d5177fa1583c2a91bba8a6cdd8295052304437b
Prashant6306 <parhad.prashant@gmail.com>
2022-12-26 23:57:37 +0530

    updated

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,27 @@
+#from sensor.constant.database import DATABASE_NAME
+DATABASE_NAME = "ineuron"
+from sensor.constant.env_variable import MONGODB_URL_KEY
+import certifi
+import os
+import pymongo
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://prashantsparhad:Numpy6306@ineuron.jd28xa9.mongodb.net/?retryWrites=true&w=majority"
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
+
+
